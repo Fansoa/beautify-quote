d4ce297a338dc919f84774638b3dbc11b591d241
dalien <daniel.cadeau13@gmail.com>
2023-02-12 15:35:01 +0000

    working version with images target path

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-async function addTextToImage(imgUrl, text, textColor) {
+const Canvas = require('canvas')
+const Image = Canvas.Image
+const fs = require('fs')
+const path = require('path')
+
+async function addTextToImage(imgUrl, filePath, text, textColor) {
   const image = await loadImage(imgUrl)
-  const canvas = createCanvas(image.width, image.height)
+  const canvas = Canvas.createCanvas(image.width, image.height)
   const ctx = canvas.getContext("2d")
   ctx.drawImage(image, 0, 0)
   ctx.font = "36px sans-serif"
   ctx.fillStyle = textColor
   ctx.textAlign = "center"
   ctx.fillText(text, canvas.width / 2, canvas.height / 2)
-  return canvas.toDataURL()
+  const data = canvas.toBuffer()
+
+  const dir = path.dirname(filePath)
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true })
+  }
+  fs.writeFileSync(filePath, data)
 }
 
 async function loadImage(url) {
@@ -21,9 +32,4 @@ async function loadImage(url) {
     }
     image.src = url
   })
-}
-
-function createCanvas(width, height) {
-  const canvas = new OffscreenCanvas(width, height)
-  return canvas
 }
